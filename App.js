0eb1dfea1a2d7a6efb09c0a861c2b0fa9bfeb7d8
Tejaswi PEDDINI (contractor) <tejaswi.peddini@amadeus.com>
2024-12-30 12:15:25 +0530

    new files

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
 const parent = React.createElement("div", {id:"parent"}, 
     React.createElement("div", {id:"child"}, 
         React.createElement('h1', {}, "I am an h1 tag")));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root1 = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(parent);
+root1.render(parent);
 
